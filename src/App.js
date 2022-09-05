7112c67bd88159da3cf47e7e94b90fc52c25fe5e
Usama Ullah <usamaullah.khan@koderlabs.com>
2022-09-06 02:29:18 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import TextForm from './Component/TextForm';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <TextForm heading="Enter Text Here" para="Simply enter your text and choose the case you want to convert it to."/>    
+    </>
   );
 }
 
---
+++ src/Component/TextForm.js
@@ -0,0 +1,75 @@
+import React, {useState} from 'react'
+
+
+
+function TextForm(props) {
+
+  const [text , setText] = useState('enter');
+
+  const handleTextField = (e) =>{
+    setText (e.target.value)
+  }
+  
+  const handleUpClick = () =>{
+    // alert('Uppercase Clicked');
+    let newText = text.toUpperCase();
+    setText(newText);
+  }
+
+  const handleLoClick = () =>{
+    // alert('Uppercase Clicked');
+    let newText = text.toLowerCase();
+    setText(newText);
+  }
+  const handleClickClear = () =>{
+    // alert('Uppercase Clicked');
+    let newText = '';
+    setText(newText);
+  }
+
+  const speak = () => {
+    let msg = new SpeechSynthesisUtterance();
+    msg.text = text;
+    window.speechSynthesis.speak(msg);
+  } 
+
+  const handleReverse = (event) => {
+    /* Convert string to array*/
+    let strArr = text.split("");
+    /* Reverse array*/
+    strArr = strArr.reverse();
+    /* Convert array to string*/
+    let newText = strArr.join("");
+    setText(newText);
+  };
+  
+  return (
+    <>
+    <div className="container">
+      <div className="form-group">
+        <h1 className=' my-3'>{props.heading}</h1>
+        <p className='my-3'>{props.para}</p>
+        <textarea className="form-control" id="my-box" value={text} onChange={handleTextField} placeholder='Type or Paster Your Content Here' rows="8"></textarea>
+
+        <button className='btn btn-primary my-3 ' onClick={handleUpClick}>Convert UpperCase</button>
+        <button className='btn btn-primary my-3 mx-3' onClick={handleLoClick}>Convert LowerCase</button>
+        <button className='btn btn-primary my-3' onClick={handleReverse}>Reverse Text</button>
+        <button className="btn btn-warning my-3 mx-3" onClick={speak} >Speak</button>
+        <button className='btn btn-danger my-3' onClick={handleClickClear}>Clear Text</button>
+
+
+      </div>
+    </div>
+
+    <div className="container">
+        <h1>Text Summary</h1>
+        <p>{text.split(" ").length} Words {text.length} Characters</p>
+        <p>{0.008 * text.split(" ").length} Minutes Reading</p>
+        <h1>Preview</h1>
+        <p>{text}</p>
+    </div>
+  </>
+  )
+}
+
+export default TextForm
